0f82060ec1e0a23b0ea8f67f519f8b46320789b9
Juan Carlos Larrondo <juancarlos1221@gmail.com>
2025-12-22 22:46:46 -0300

    Add HealthController to AppModule controllers
    Registers HealthController in the AppModule controllers array to ensure it is properly recognized and used by NestJS.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { GameGateway } from './game.gateway';
-import { HealthController } from './health.controller';
-import { RoomsService } from './rooms.service';
+import { Module } from "@nestjs/common";
+import { GameGateway } from "./game.gateway";
+import { HealthController } from "./health.controller";
+import { RoomsService } from "./rooms.service";
 
 @Module({
   providers: [RoomsService, GameGateway],
+  controllers: [HealthController],
 })
 export class AppModule {}
